1efa92e57373a7fc89d777153a3ef38580b3bec4
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2017-12-19 17:57:28 +0100

    feature(docker): update sidecar script to use required endpoints, use POST method first, if already available use PUT

--- docker/PlatformSidecars/app-registration-mongodb/sidecar.sh
+++ docker/PlatformSidecars/app-registration-mongodb/sidecar.sh
@@ -8,32 +8,54 @@ echo $YP_ADDRESS
 
 #check if YP is up and running
 STR='"health": "false"'
-STR=$(curl -sb -H "Accept: application/json" "http://$YP_ADDRESS/health")
-while [[ $STR != *'"health": "true"'* ]]
+STR=$(curl -k -s -H "Accept: application/json" "$YP_ADDRESS/api/v1/health")
+while [[ $STR != *'"health":"true"'* ]]
 do
 	echo "Waiting for YP ..."
-	STR=$(curl -sb -H "Accept: application/json" "http://$YP_ADDRESS/health")
-	sleep 1
+	STR=$(curl -k -s -H "Accept: application/json" "$YP_ADDRESS/api/v1/health")
+	sleep 10
 done
 
-#Register Application
-curl -X PUT "http://$YP_ADDRESS/v1/apps/$APP_NAME" \
-    -H "Content-Type: application/json" \
-    --data @<(generate_post_data)
+echo "response of GET /api/v1/health: " $STR
 
 # function to generate post data
 generate_post_data() {
 cat <<EOF
 {
-    "id":"$APP_NAME",
-    "url":"$APP_URL",
-    "description":"$APP_DESCRIPTION",
-    "icon":"$APP_ICON",
-    "lifecycleStatus":"online",
-    "appType":"$APP_TYPE"
- }
+  "id":"$APP_NAME",
+  "url":"$APP_URL",
+  "description":"$APP_DESCRIPTION",
+  "icon":"$APP_ICON",
+  "lifecycleStatus":"online",
+  "appType":"$APP_TYPE",
+  "category":"productivity"
+}
 EOF
 }
+
+echo $(generate_post_data)
+
+#Register Application
+response=$(curl -X POST -k -i -f \
+                --write-out %{http_code} --silent --output /dev/null \
+                -H "Accept: application/json" \
+                -H "Content-Type: application/json" \
+                --data "$(generate_post_data)" \
+                "$YP_ADDRESS/api/v1/apps")
+
+echo "response of POST /api/v1/apps: " $response
+
+if [ "$response" != 200 ]
+    then
+      echo '{"lifecycleStatus":"online"}'
+      echo "response of PUT /api/v1/apps/$APP_NAME: " \
+           $(curl -X PUT -k -i -f \
+                  --write-out %{http_code} --silent --output /dev/null \
+                  -H "Accept: application/json" \
+                  -H "Content-Type: application/json" \
+                  -d '{"lifecycleStatus":"online"}' \
+                  "$YP_ADDRESS/api/v1/apps/$APP_NAME")
+fi
 
 # SIGTERM-handler
 # Unregister this application on ctr+c
@@ -41,9 +63,14 @@ term_handler() {
   echo "[Sidecar] Shutting Down"
 
   #Set Status Offline
-  curl -L -X PUT "http://$YP_ADDRESS/v1/apps/$APP_NAME" \
-       -H "Content-Type: application/json" \
-       -d '{"lifecycleStatus":"offline"}'
+  echo '{"lifecycleStatus":"offline"}'
+  echo "response of PUT /api/v1/apps/$APP_NAME: " \
+        $(curl -L -X PUT -k -i -f \
+               --write-out %{http_code} --silent --output /dev/null \
+               -H "Accept: application/json" \
+               -H "Content-Type: application/json" \
+               -d '{"lifecycleStatus":"offline"}' \
+               "$YP_ADDRESS/api/v1/apps/$APP_NAME")
 
   exit 143; # 128 + 15 -- SIGTERM
 }
